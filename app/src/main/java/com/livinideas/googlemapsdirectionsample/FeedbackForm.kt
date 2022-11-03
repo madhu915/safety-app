2bebbb4de69d0d277781cdfb469ad1d466db5cf2
Madhu <mmadhumithaa09@gmail.com>
2022-11-04 04:07:56 +0530

    feat: route info window added -display route info on demand -feedback form logic completed

--- app/src/main/java/com/livinideas/googlemapsdirectionsample/FeedbackForm.kt
+++ app/src/main/java/com/livinideas/googlemapsdirectionsample/FeedbackForm.kt
@@ -1,22 +1,25 @@
 package com.livinideas.googlemapsdirectionsample
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
+import kotlin.collections.HashMap
 
-class FeedbackForm: AppCompatActivity() {
-    lateinit var db: DocumentReference
+class FeedbackForm : AppCompatActivity() {
+    lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feedback)
 
-        db = FirebaseFirestore.getInstance().document("FeedbackForm/User")
+        db = FirebaseFirestore.getInstance()
 
         val submit = findViewById<View>(R.id.submit) as Button
 
@@ -26,43 +29,82 @@ class FeedbackForm: AppCompatActivity() {
     }
 
     private fun Submit() {
-        val user=findViewById<View>(R.id.user) as EditText
+        val user = findViewById<View>(R.id.user) as EditText
         val area = findViewById<View>(R.id.area) as EditText
         val pc = findViewById<View>(R.id.pc) as EditText
         val bar = findViewById<View>(R.id.bar) as EditText
         val time = findViewById<View>(R.id.time) as EditText
         val pf = findViewById<View>(R.id.pf) as EditText
-        val zone=findViewById<View>(R.id.zone) as EditText
+        val zone = findViewById<View>(R.id.zone) as EditText
 
-        val User=user.text.toString().trim()
+        val User = user.text.toString().trim()
         val Area1 = area.text.toString().trim()
         val PC1 = pc.text.toString().trim()
         val BAR1 = bar.text.toString().trim()
         val TIME1 = time.text.toString().trim()
         val PF1 = pf.text.toString().trim()
-        val zone1=zone.text.toString().trim()
+        val zone1 = zone.text.toString().trim()
 
         if (!User.isEmpty() && !Area1.isEmpty() && !PC1.isEmpty() && !BAR1.isEmpty() && !TIME1.isEmpty() && !PF1.isEmpty() && !zone1.isEmpty()) {
             try {
-                val items = HashMap<String, Any>()
-                items.put("Is Police_Station", PC1)
-                items.put("Is Bar", BAR1)
-                items.put("Time", TIME1)
-                items.put("People Frequency", PF1)
-                items.put("Area",Area1)
-                items.put("Zone",zone1)
+                data class Fields(
+                    val area: String,
+                    val is_bar: String,
+                    val isPolice_Station: String,
+                    val peopleFrequency: String,
+                    val time: String,
+                    val zone: String,
+                )
+
+                val items = Fields(Area1, BAR1, PC1, PF1, TIME1, zone1)
+
+                val data: HashMap<String, Any> = HashMap()
+                data.put("feed", Arrays.asList(items))
+                db.collection("Feedback").document("${User}").get().addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val document = task.result
+                        if (document != null) {
+                            if (document.exists()) {
+                                Log.d("TAG", "Document already exists.")
+
+                                db.collection("Feedback").document("${User}")
+                                    .update("feed", FieldValue.arrayUnion(items))
+                                    .addOnSuccessListener { void: Void? ->
+                                        Toast.makeText(this,
+                                            "Successfully uploaded",
+                                            Toast.LENGTH_LONG).show()
+                                        user.text.clear()
+                                        area.text.clear()
+                                        pc.text.clear()
+                                        bar.text.clear()
+                                        time.text.clear()
+                                        pf.text.clear()
+                                        zone.text.clear()
+                                    }
+                            } else {
+                                Log.d("TAG", "Document doesn't exist.")
+                                db.collection("Feedback").document("${User}").set(data)
+                                    .addOnSuccessListener { void: Void? ->
+                                        Toast.makeText(this,
+                                            "Successfully uploaded",
+                                            Toast.LENGTH_LONG).show()
+                                        user.text.clear()
+                                        area.text.clear()
+                                        pc.text.clear()
+                                        bar.text.clear()
+                                        time.text.clear()
+                                        pf.text.clear()
+                                        zone.text.clear()
 
 
-                db.collection(User).document("${User}_${zone1}_${TIME1}").set(items).addOnSuccessListener {
-                        void: Void? ->
-                    Toast.makeText(this,"Successfully uploaded", Toast.LENGTH_LONG).show()
-                    user.text.clear()
-                    area.text.clear()
-                    pc.text.clear()
-                    bar.text.clear()
-                    time.text.clear()
-                    pf.text.clear()
-                    zone.text.clear()
+                                    }
+                            }
+                        } else {
+                            Log.d("TAG", "Error: ", task.exception)
+                        }
+
+                    }
+
                 }
             } catch (e: Exception) {
                 Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
--- app/src/main/java/com/livinideas/googlemapsdirectionsample/MainActivity.kt
+++ app/src/main/java/com/livinideas/googlemapsdirectionsample/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.livinideas.googlemapsdirectionsample
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import android.location.Address
 import android.location.Geocoder
+import android.os.Build
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.method.ScrollingMovementMethod
+import android.text.style.ForegroundColorSpan
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
@@ -19,17 +27,21 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.maps.android.PolyUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-
+import java.time.LocalDateTime
+import java.util.*
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private var googleMap: GoogleMap? = null
+    lateinit var db: FirebaseFirestore
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,8 +56,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 //            myIntent.putExtra("key1", "login")
             this.startActivity(myIntent)
         }
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.fragment_map) as SupportMapFragment
+        val mapFragment = 
+            supportFragmentManager.findFragmentById(R.id.fragment_map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
@@ -53,9 +67,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 //        added clear view option
         clearBtn.setOnClickListener {
             this.googleMap!!.clear()
+            routeInfo.isVisible = !routeInfo.isVisible
         }
+
 //        added search route option
         searchBtn.setOnClickListener {
+            routeInfo.isVisible = !routeInfo.isVisible
             var start = startAddress.text.toString()
             var destination = destinationAddress.text.toString()
             Log.d("TAG", "onMapReady: $start")
@@ -65,18 +82,24 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             var latLngDestination = getLocationFromAddress(destination)
 
             this.googleMap!!.addMarker(MarkerOptions().position(latLngOrigin).title(start))
-            this.googleMap!!.addMarker(MarkerOptions().position(latLngDestination).title(destination))
+            this.googleMap!!.addMarker(MarkerOptions().position(latLngDestination)
+                .title(destination))
             this.googleMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngOrigin, 14.5f))
 //            val path: MutableList<List<LatLng>> = ArrayList()
-            val urlDirections = "https://maps.googleapis.com/maps/api/directions/json?origin=${latLngOrigin?.latitude},${latLngOrigin?.longitude}&destination=${latLngDestination?.latitude},${latLngDestination?.longitude}&mode=driving&alternatives=true&key=AIzaSyDxRfxqFdgUlccCFu65mDq2C9Ao33G9q4A"
+            val urlDirections = 
+                "https://maps.googleapis.com/maps/api/directions/json?origin=${latLngOrigin?.latitude},${latLngOrigin?.longitude}&destination=${latLngDestination?.latitude},${latLngDestination?.longitude}&mode=driving&alternatives=true&key=AIzaSyDxRfxqFdgUlccCFu65mDq2C9Ao33G9q4A"
 //            Log.d("latlg","${latLngDestination?.latitude}+${latLngDestination?.longitude}")
-            val directionsRequest = object : StringRequest(Request.Method.GET, urlDirections, Response.Listener<String> {
-                    response ->
-                val jsonResponse = JSONObject(response)
+            val directionsRequest = @SuppressLint("SetTextI18n")
+            object : StringRequest(Request.Method.GET, 
+                urlDirections, 
+                Response.Listener<String> { response ->
+                    val jsonResponse = JSONObject(response)
                 // Get routes
                 val routes = jsonResponse.getJSONArray("routes")
-                Log.d("size","${routes.length()}+${latLngOrigin?.latitude}+${latLngOrigin?.longitude}+${latLngDestination?.latitude}+${latLngDestination?.longitude}")
+                Log.d("size",
+                    "${routes.length()}+${latLngOrigin?.latitude}+${latLngOrigin?.longitude}+${latLngDestination?.latitude}+${latLngDestination?.longitude}")
                 //maximum five possible routes
+                
                 if (routes.length()==1) {
                     Log.d("size", "${routes.getJSONObject(0).get("summary")}")
                     val legs = routes.getJSONObject(0).getJSONArray("legs")
@@ -85,7 +108,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
                     //log ETA and distance
 
+                    var result = ""
+
                     Log.d("mul", "${dist}+${eta}")
+                    result = StringBuilder().append("Distance covered: ").append(dist)
+                        .append(" ETA: ").append(eta).append("\n").toString()
+
                     val steps = legs.getJSONObject(0).getJSONArray("steps")
                     val path: MutableList<List<LatLng>> = ArrayList()
                     for (i in 0 until steps.length()) {
@@ -99,11 +127,44 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                             .color(Color.RED))
 
                     }
+                    val texts = findViewById<TextView>(R.id.routeInfo)
+                    texts.movementMethod = ScrollingMovementMethod()
+                    Log.d("ress", "${result}")
+
+                    val word: Spannable = SpannableString(result)
+
+                    word.setSpan(ForegroundColorSpan(Color.parseColor("#FF8BC34A")),
+                        0,
+                        word.length,
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+                    texts.setText(word)
                 }
+
                 if(routes.length()>1) {
                     val cols =
-                        arrayOf(Color.RED, Color.BLUE, Color.BLACK, Color.CYAN, Color.MAGENTA)
+                        arrayOf(Color.parseColor("#FF8BC34A"),
+                                Color.RED,
+                                Color.BLUE,
+                                Color.BLACK,
+                                Color.CYAN)
                     var counter=-1
+                    var mresult: String = ""
+
+                    val texts = findViewById<TextView>(R.id.routeInfo)
+                    val word: Spannable = SpannableString(mresult)
+
+                    word.setSpan(ForegroundColorSpan(Color.parseColor("#FF8BC34A")),
+                        0,
+                        word.length,
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+                    texts.setText(word)
+
+                    //index for saferoute
+                    val res = findSafestRoute(routes)
+
+
                     for (j in 0 until routes.length()) {
                         //maximum 5 routes allowed
                         if(j==5){
@@ -111,32 +172,50 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                         }
                         val jsonObject = routes.getJSONObject(j)
                         val legs = jsonObject.getJSONArray("legs")
-                        val dist= legs.getJSONObject(0).getJSONObject("distance").getString("text")
-                        val eta=legs.getJSONObject(0).getJSONObject("duration").getString("text")
+                        val dist= 
+                            legs.getJSONObject(0).getJSONObject("distance").getString("text")
+                        val eta=
+                            legs.getJSONObject(0).getJSONObject("duration").getString("text")
 
                         //log ETA and distance
 
                         Log.d("mul","${dist}+${eta}")
                         counter += 1
+
+                            val routeInfo =
+                                StringBuilder().append("Distance covered: ").append(dist)
+                                    .append(" ETA: ").appendLine(eta).appendLine().toString()
+                            val wordTwo: Spannable = SpannableString(routeInfo)
+
+                            wordTwo.setSpan(ForegroundColorSpan(cols[counter]),
+                                0,
+                                wordTwo.length,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                            texts.append(wordTwo)
+
+
+
                         Log.d("countss","${counter}")
                         val steps = legs.getJSONObject(0).getJSONArray("steps")
                         val path: MutableList<List<LatLng>> = ArrayList()
 
                         for (i in 0 until steps.length()) {
                             val points =
-                                steps.getJSONObject(i).getJSONObject("polyline").getString("points")
+                                steps.getJSONObject(i).getJSONObject("polyline")
+                                    .getString("points")
                             path.add(PolyUtil.decode(points))
                         }
                         Log.d("psize", "${path.size}")
                         for (i in 0 until path.size) {
-                            this.googleMap!!.addPolyline(PolylineOptions().addAll(path[i]).color(cols[counter]))
+                            this.googleMap!!.addPolyline(PolylineOptions().addAll(path[i])
+                                .color(cols[counter]))
                         }
                     }
                 }
 
-            }, Response.ErrorListener {
-                    _ ->
-            }){}
+            }, 
+            Response.ErrorListener { _ ->
+            }) {}
 
             val requestQueue = Volley.newRequestQueue(this)
             requestQueue.add(directionsRequest)
